ac8b3657bf76dcf811b5a82f67e1491d9cdded80
= <mtagius@gmail.com>
2019-02-22 23:08:47 -0500

    added more basic content

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var graduationDate = new Date('May 11, 2019');
-    if(Date.now() > graduationDate) {
-        $("#graduationDate").html("graduated");
-        $("#graduationTense").html("ed");
+    if(Date.now() < graduationDate) {
+        $("#graduationDate").html("will graduate in May of 2019");
+        $("#graduationTense").html("");
     }
 });
