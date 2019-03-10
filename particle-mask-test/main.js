472513ed7755b74c6ca56d188a92c56f6cc9d1c3
Matthew Agius <mtagius@gmail.com>
2019-03-10 12:03:55 -0400

    particle test

---
+++ particle-mask-test/main.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function() {
+    $('#dots').particleground({
+        dotColor: '#000000',
+        lineColor: '#000000',
+        density: 10000,
+        minSpeedX: .1,
+        maxSpeedX: .2,
+        minSpeedY: .1,
+        maxSpeedY: .2,
+        particleRadius: 100,
+        parallaxMultiplier: 15,
+        proximity: 10
+    });
+
+
+    $('#particles').particleground({
+        dotColor: '#000000',
+        lineColor: '#000000',
+        density: 10000,
+        minSpeedX: .1,
+        maxSpeedX: .5,
+        minSpeedY: .1,
+        maxSpeedY: .5,
+        particleRadius: 15,
+        parallaxMultiplier: 15,
+        proximity: 100
+    });
+
+
+});
