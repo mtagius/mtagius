075158f83c08d744809f346c16b3129cd429e4fe
Matthew Agius <mtagius@gmail.com>
2019-03-10 23:37:14 -0400

    fixed the masking on the particle effects

--- particle-mask-test/main.js
+++ particle-mask-test/main.js
@@ -4,16 +4,15 @@ $(document).ready(function() {
     $('#dots').particleground({
         dotColor: '#000000',
         lineColor: '#000000',
-        density: 10000,
+        density: 80000,
         minSpeedX: .1,
-        maxSpeedX: .2,
+        maxSpeedX: .5,
         minSpeedY: .1,
-        maxSpeedY: .2,
-        particleRadius: 100,
-        parallaxMultiplier: 15,
+        maxSpeedY: .5,
+        particleRadius: 200,
+        parallaxMultiplier: 100,
         proximity: 10
     });
-
 
     $('#particles').particleground({
         dotColor: '#000000',
