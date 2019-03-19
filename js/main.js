0e5666a0cee058852cc300ac79586fc8a0a85a50
= <mtagius@gmail.com>
2019-03-19 19:25:18 -0400

    merged the 2 designs

--- js/main.js
+++ js/main.js
@@ -4,4 +4,30 @@ $(document).ready(function() {
         $("#graduationDate").html("will graduate in May of 2019");
         $("#graduationTense").html("");
     }
+
+    $('#dots').particleground({
+        dotColor: '#000000',
+        lineColor: '#000000',
+        density: 80000,
+        minSpeedX: .1,
+        maxSpeedX: .5,
+        minSpeedY: .1,
+        maxSpeedY: .5,
+        particleRadius: 200,
+        parallaxMultiplier: 100,
+        proximity: 10
+    });
+
+    $('#particles').particleground({
+        dotColor: '#000000',
+        lineColor: '#000000',
+        density: 10000,
+        minSpeedX: .1,
+        maxSpeedX: .5,
+        minSpeedY: .1,
+        maxSpeedY: .5,
+        particleRadius: 15,
+        parallaxMultiplier: 15,
+        proximity: 100
+    });
 });
